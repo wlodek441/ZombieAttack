641700d644a6b3fd82439fd1e5828dff95b7b803
Kamil Włodkowski <68141505+wlodek441@users.noreply.github.com>
2022-12-22 22:39:21 +0100

    Add bullet movement

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class BulletMovement : MonoBehaviour
 {
+    public float speed;
+    public Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();  
+        rigidbody2D.velocity = transform.right * speed;
+
+    }
     // Start is called before the first frame update
     void Start()
     {
